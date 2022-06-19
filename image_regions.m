95c87f9439e07b5c40e7399e03fcda109ce857b2
Im-Rises <quentin-morel88@hotmail.com>
2022-06-19 23:00:52 +0200

    Corrected image regiosn videos axisd

--- image_regions.m
+++ image_regions.m
@@ -2,6 +2,6 @@ function imageRegions = image_regions(image)
     imageLabeled = bwlabel(image); % Label the cells
     nombreCells = max(max(imageLabeled)); % Calculate the number of cells (number of connected spaces)
     imagesc(imageLabeled), title(['Image regions, number of cells ', num2str(nombreCells)]);
-    %imageRegions = imageLabeled;
+    axis off
     imageRegions = getframe(gcf).cdata;
 end
