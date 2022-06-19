4e94ae2ae146a542cc49a11ada890f8a3370f53d
Im-Rises <quentin-morel88@hotmail.com>
2022-06-19 23:37:54 +0200

    Corrected number of cell issue for segmentation version

--- image_segmentation.m
+++ image_segmentation.m
@@ -1,7 +1,7 @@
 function imageSegmented = image_segmentation(image)
     imageLabeled = bwlabel(image); % Label the cells
     nombreCells = max(max(imageLabeled)); % Calculate the number of cells (number of connected spaces)    
-    imagesc(imageLabeled), title(['Image segmented, number of cells ', num2str(nombreCells)]);
+    imagesc(imageLabeled);
     B = bwboundaries(image);
     imlabel = bwlabel(image);
     imshow(image); hold on
@@ -9,5 +9,6 @@ function imageSegmented = image_segmentation(image)
         b = B{k};
         plot(b(:,2),b(:,1),'g','linewidth',2);
     end
+    title(['Image segmented, number of cells ', num2str(nombreCells)]);
     imageSegmented = getframe(gcf).cdata;
 end
